5f8f2ff05a2bc76ff228066516964807d9f62468
DhimasArbi <dhimas.arbi@gmail.com>
2021-10-11 16:49:53 +0700

    Activity 1

--- MyCaffe/app/src/main/java/org/aplas/mycaffe/MainActivity.java
+++ MyCaffe/app/src/main/java/org/aplas/mycaffe/MainActivity.java
@@ -1,14 +1,67 @@
 package org.aplas.mycaffe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rcView;
+    public DataAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rcView = (RecyclerView) findViewById(R.id.dataView);
+        rcView.setLayoutManager(new LinearLayoutManager(this));
+        loadCaffeData();
+    }
+
+    public static int getId(String resourceName, Class<?> c) {
+        try {
+            Field idField = c.getDeclaredField(resourceName);
+            return idField.getInt(idField);
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    private ArrayList<DataItem> getCaffeData() {
+        //Get the resources from the XML file
+        String[] listTitles = getResources().getStringArray(R.array.caffe_title);
+//        int[] listColor = getResources().getIntArray(R.array.menu_color);
+        String[] listInfo = getResources().getStringArray(R.array.caffe_info);
+        String[] listIcon = getResources().getStringArray(R.array.caffe_icon);
+        ArrayList<DataItem> data = new ArrayList<>();
+        //Create the ArrayList with the titles and information
+        for (int i=0; i<listTitles.length; i++) {
+            data.add(new DataItem(listTitles[i], getId(listIcon[i],R.drawable.class)));
+        }
+        return data;
+    }
+    private void loadCaffeData() {
+        //Initialize the adapter and set it ot the RecyclerView
+        mAdapter = new DataAdapter(this, getCaffeData());
+        ItemTouchHelper.Callback callback = new ItemMoveCallback(mAdapter);
+        ItemTouchHelper touchHelper = new ItemTouchHelper(callback);
+        touchHelper.attachToRecyclerView(rcView);
+        rcView.setAdapter(mAdapter);
+        //Notify the adapter of the change
+        mAdapter.notifyDataSetChanged();
+    }
+
+    private void openMediaActivity(String title) {
+        Intent media = new Intent(getApplicationContext(),OrderActivity.class);
+        startActivity(media);
     }
 }
