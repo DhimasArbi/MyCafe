e5f6b3fe033d3cae670b3f047d11b7c9f96808cf
ArifArfi <arifarfi35@gmail.com>
2021-11-25 12:21:04 +0700

    edit mainactivity

--- SanaSiniCoffee/app/src/main/java/org/aplas/sanasinicoffee/MainActivity.java
+++ SanaSiniCoffee/app/src/main/java/org/aplas/sanasinicoffee/MainActivity.java
@@ -1,14 +1,25 @@
 package org.aplas.sanasinicoffee;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        public void ButtonOnClick(View V){
+            textview.setText("Button telah di Klik");
+            textview.setVisibility(View.VISIBLE);
+        }
     }
 }
