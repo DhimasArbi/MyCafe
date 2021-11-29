7a33fbc4f20c0f3874a34ebcac16fa466d9bc1e6
DhimasArbi <dhimas.arbi@gmail.com>
2021-11-29 20:54:55 +0700

    update

--- SanaSiniCoffee/app/src/main/java/org/aplas/sanasinicoffee/MainActivity.java
+++ SanaSiniCoffee/app/src/main/java/org/aplas/sanasinicoffee/MainActivity.java
@@ -15,10 +15,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textview = (TextView) findViewById(R.id.textView);
     }
-        public void ButtonOnClick(View V){
-            textview.setText("Button telah di Klik");
-            textview.setVisibility(View.VISIBLE);
-        }
     }
